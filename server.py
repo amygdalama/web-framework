634fef0f43877999e317d9e2098adee4c84bf6c2
amygdalama <amyehanlon@gmail.com>
2014-05-25 15:07:49 -0400

    function routing!!!

--- server.py
+++ server.py
@@ -5,29 +5,51 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 PORT = 8000
 
 class MyHandler(BaseHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        self.router = Router(self)
+        self.router.add('hello', hello)
+        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
 
     def do_GET(self):
         try:
-            filename = map_url(self.path)
-            f = open(filename)
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
+            # os assumes paths begining with '/' are absolute
+            if self.path.startswith('/'):
+                self.path = self.path[1:]
+
+            func = self.router.route(self.path)
+            if func:
+                func(self)
+            else:
+                if self.path == '' or os.path.isdir(self.path):
+                    filename = os.path.join(self.path, 'index.html')
+                else:
+                    filename = self.path
+                f = open(filename)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
         except IOError:
             self.send_error(404, 'File not found: %s' % filename)
         return
 
-def map_url(path):
-    # os assumes paths begining with '/' are absolute
-    if path.startswith('/'):
-        path = path[1:]
-    if path == '' or os.path.isdir(path):
-        filename = os.path.join(path, 'index.html')
-    else:
-        filename = path
-    return filename
+class Router(object):
+    def __init__(self, server):
+        self.routes = {}
+        self.server = server
+
+    def add(self, route, func):
+        self.routes[route] = func
+
+    def route(self, route):
+        return self.routes.get(route, None)
+
+def hello(server):
+    server.send_response(200)
+    server.send_header('Content-type', 'text/html')
+    server.end_headers()
+    server.wfile.write("Hello world from routed function land!!!!")
 
 def parse_args():
     parser = argparse.ArgumentParser()
